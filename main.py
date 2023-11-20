40f7d8688500be344c6536f6d2fb6a4b9161533c
kishrajesh <kirajesh@hmc.edu>
2023-11-19 16:34:58 -0800

    randommm color

--- main.py
+++ main.py
@@ -27,6 +27,10 @@ def main():
   
   drag = False
   
+  r = 0
+  g = 0
+  b = 0
+
   while 1:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -55,13 +59,34 @@ def main():
     #https://www.pygame.org/docs/ref/draw.html#pygame.draw.polygon
     # ^ for drawing the cloth
 
+    if(random.random() > 0.5):
+      r+=1
+      r%=255
+    else:
+      r-=1
+      r%=255
+    if(random.random() > 0.5):
+      g+=1
+      g%=255
+    else:
+      g-=1
+      g%=255
+    if(random.random() > 0.5):
+      b+=1
+      b%=255
+    else:
+      b-=1
+      b%=255
     for i in range(len(cloth.points)-1):
       for j in range(len(cloth.points[0])-1):
         v1 = (cloth.points[i][j].x, cloth.points[i][j].y)
         v2 = (cloth.points[i+1][j].x, cloth.points[i+1][j].y)
         v3 = (cloth.points[i+1][j+1].x, cloth.points[i+1][j+1].y)
         v4 = (cloth.points[i][j+1].x, cloth.points[i][j+1].y)
-        pygame.draw.polygon(screen, (255*random.random(),255*random.random(),255*random.random()), (v1,v2,v3,v4), 0)
+        
+        
+
+        pygame.draw.polygon(screen, (r,g,b), (v1,v2,v3,v4), 0)
     
     for link in cloth.links:
       pygame.draw.line(screen, (255,255,255), (link.p1.x,link.p1.y), (link.p2.x,link.p2.y), 2)
