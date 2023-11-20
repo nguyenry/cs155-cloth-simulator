11a57fffa76ee3105a4b6e203b5c4abab0a7717a
kishrajesh <kirajesh@hmc.edu>
2023-11-20 15:08:26 -0800

    colors

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def main():
   
   pygame.event.set_allowed([pygame.QUIT, pygame.MOUSEBUTTONDOWN, pygame.MOUSEBUTTONUP, pygame.KEYDOWN])
   
-  cloth = Cloth(size=50, l=10, tear=1000, offset=(100,20), screen_size=(screen_w,screen_h))
+  cloth = Cloth(size=50, l=10, tear=5000, offset=(100,20), screen_size=(screen_w,screen_h))
   
 
   drag = False
@@ -48,7 +48,7 @@ def main():
         cloth.end_drag()
       elif event.type == pygame.KEYDOWN:
         if event.key == pygame.K_r:
-          cloth = Cloth(size=50, l=10, tear=10000,offset=(100,20), screen_size=(screen_w,screen_h))
+          cloth = Cloth(size=50, l=10, tear=5000,offset=(100,20), screen_size=(screen_w,screen_h))
     
     mouse_pos = pygame.mouse.get_pos()
     
@@ -64,20 +64,21 @@ def main():
     # ^ for drawing the cloth
     r,g,b = 100,150,200
 
-    #2d array of colors, fractal noise
+    #2d array of colors
     colors = np.zeros((len(cloth.points),len(cloth.points[0]),3))
     for i in range(len(cloth.points)):
       for j in range(len(cloth.points[0])):
-        colors[i][j] = [i/len(cloth.points),j/len(cloth.points[0]),0]
-
-
+        colors[i][j] = [i/len(cloth.points),j/len(cloth.points[0]),0.5]
+    
     for i in range(len(cloth.points)-1):
       for j in range(len(cloth.points[0])-1):
         v1 = (cloth.points[i][j].x, cloth.points[i][j].y)
         v2 = (cloth.points[i+1][j].x, cloth.points[i+1][j].y)
         v3 = (cloth.points[i+1][j+1].x, cloth.points[i+1][j+1].y)
         v4 = (cloth.points[i][j+1].x, cloth.points[i][j+1].y)
-        (r,g,b) = colors[i][j]*255
+        (r,g,b) = (v1[0]/screen_w,v1[1]/screen_h,0.5)
+        (r,g,b) = (255*r,255*g,255*b)
+
         pygame.draw.polygon(screen, (r,g,b), (v1,v2,v3,v4), 0)
         
     
